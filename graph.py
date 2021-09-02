2f01c8138ac351a543db838e2d741c227eaa293e
Meemidh <71092146+Meemidh@users.noreply.github.com>
2021-09-02 15:03:06 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -47,7 +47,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.H1("Demo: Plotly Express in Dash with Tips Dataset"),
+    html.H1("Demo: Plotly Express in Dash with Sentiment Dataset"),
     
     dcc.DatePickerRange(
         id='my-date-picker-range',
